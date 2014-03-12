ff76d81bffdf875ba513eb6e6ca1be3698a6cad7
dev-zzo <gauri@tut.by>
2014-03-12 18:03:37 +0100

    Fixed naming for BaseClassDescriptor2

--- IDAHacks.py
+++ IDAHacks.py
@@ -1,5 +1,36 @@
 import idc
 
+def toSigned(v, bits) :
+	print "%d/%d" % (v, bits)
+	if (v & (1 << (bits - 1))) != 0 :
+		return -((~v + 1) & ((1 << bits) - 1))
+	return v
+#
+
+def getUInt8(ea) :
+	return idc.Byte(ea)
+#
+
+def getInt8(ea) :
+	return toSigned(idc.Byte(ea), 8)
+#
+
+def getUInt16(ea) :
+	return idc.Word(ea)
+#
+
+def getInt16(ea) :
+	return toSigned(idc.Word(ea), 16)
+#
+
+def getUInt32(ea) :
+	return idc.Dword(ea)
+#
+
+def getInt32(ea) :
+	return toSigned(idc.Dword(ea), 32)
+#
+
 def getAsciiz(ea) :
 	str = ""
 	while idc.Byte(ea) != 0:
--- Msvc/Rtti/BaseClassDescriptor2.py
+++ Msvc/Rtti/BaseClassDescriptor2.py
@@ -1,8 +1,21 @@
+import string
 import idc
 import IDAHacks
 import RttiError
 IDAHacks = reload(IDAHacks)
 RttiError = reload(RttiError)
+
+def mangleNumber(num) :
+	if num == 0 :
+		return "@"
+	sign = ""
+	if num < 0 :
+		sign = "?"
+		num = -num
+	numtext = hex(num).upper()[2:-1]
+	print numtext
+	numtext = sign + numtext.translate(string.maketrans('0123456789ABCDEF', 'ABCDEFGHIJKLMNOP')) + "@"
+	return numtext
 
 class BaseClassDescriptor2 :
 	"""
@@ -26,20 +39,25 @@ class BaseClassDescriptor2 :
 		# RTTIClassHierarchyDescriptor *pClassDescriptor;
 		
 		self.typeDescriptor = rtti.getTypeDescriptor(idc.Dword(ea))
-		baseCount = idc.Dword(ea + 4)
-		self.mdisp = idc.Dword(ea + 8)
-		self.pdisp = idc.Dword(ea + 12)
-		self.vdisp = idc.Dword(ea + 16)
-		self.attributes = idc.Dword(ea + 20)
+		baseCount = IDAHacks.getUInt32(ea + 4)
+		self.mdisp = IDAHacks.getInt32(ea + 8)
+		self.pdisp = IDAHacks.getInt32(ea + 12)
+		self.vdisp = IDAHacks.getInt32(ea + 16)
+		self.attributes = IDAHacks.getUInt32(ea + 20)
 		
 		# Define data in DB
 		IDAHacks.undefBytes(ea, 28)
 		idc.MakeStructEx(ea, -1, "_s__RTTIBaseClassDescriptor2")
 		
-		# Make a name, if not there ??_R1 A@ ?0A@ EA@ {class}@@8
-		#name = "??_R0?AV" + self.name + "@@@8"
-		#if name != idc.Name(ea) :
-		#	idc.MakeNameEx(ea, name, 0)
+		# Make a name, if not there
+		name = "??_R1"
+		name += mangleNumber(self.mdisp)
+		name += mangleNumber(self.pdisp)
+		name += mangleNumber(self.vdisp)
+		name += mangleNumber(self.attributes)
+		name += self.typeDescriptor.name + "@@8"
+		if name != idc.Name(ea) :
+			idc.MakeNameEx(ea, name, 0)
 	# End of __init__()
 	
 	def __str__(self) :
@@ -56,8 +74,8 @@ if id : idc.DelStruc(id)
 id = idc.AddStrucEx(-1, "_s__RTTIBaseClassDescriptor2", 0);
 idc.AddStrucMember(id,"pTypeDescriptor",	0,	0x25500400,	0XFFFFFFFF,	4,	0XFFFFFFFF,	0,	0x000002);
 idc.AddStrucMember(id,"numContainedBases",	0X4,	0x20000400,	-1,	4);
-idc.AddStrucMember(id,"mdisp",	0X8,	0x20000400,	-1,	4);
-idc.AddStrucMember(id,"pdisp",	0XC,	0x20000400,	-1,	4);
-idc.AddStrucMember(id,"vdisp",	0X10,	0x20000400,	-1,	4);
+idc.AddStrucMember(id,"mdisp",	0X8,	0x20200400,	-1,	4);
+idc.AddStrucMember(id,"pdisp",	0XC,	0x20200400,	-1,	4);
+idc.AddStrucMember(id,"vdisp",	0X10,	0x20200400,	-1,	4);
 idc.AddStrucMember(id,"attributes",	0X14,	0x20000400,	-1,	4);
 idc.AddStrucMember(id,"pClassDescriptor",	0X18,	0x25500400,	0XFFFFFFFF,	4,	0XFFFFFFFF,	0,	0x000002);
