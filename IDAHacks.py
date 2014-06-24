30c967964c5e90ef6b448dd954ad50d0c11b59a6
dev-zzo <gauri@tut.by>
2014-06-24 20:27:18 +0200

    Reworked RTTI stuff, but needs more work on automation.

--- IDAHacks.py
+++ IDAHacks.py
@@ -1,56 +1,56 @@
 import idc
 
 def toSigned(v, bits) :
-	if (v & (1 << (bits - 1))) != 0 :
-		return -((~v + 1) & ((1 << bits) - 1))
-	return v
+    if (v & (1 << (bits - 1))) != 0 :
+        return -((~v + 1) & ((1 << bits) - 1))
+    return v
 #
 
 def getUInt8(ea) :
-	return idc.Byte(ea)
+    return idc.Byte(ea)
 #
 
 def getInt8(ea) :
-	return toSigned(idc.Byte(ea), 8)
+    return toSigned(idc.Byte(ea), 8)
 #
 
 def getUInt16(ea) :
-	return idc.Word(ea)
+    return idc.Word(ea)
 #
 
 def getInt16(ea) :
-	return toSigned(idc.Word(ea), 16)
+    return toSigned(idc.Word(ea), 16)
 #
 
 def getUInt32(ea) :
-	return idc.Dword(ea)
+    return idc.Dword(ea)
 #
 
 def getInt32(ea) :
-	return toSigned(idc.Dword(ea), 32)
+    return toSigned(idc.Dword(ea), 32)
 #
 
 def getAsciiz(ea) :
-	str = ""
-	while idc.Byte(ea) != 0:
-		str = str + "%c" % idc.Byte(ea)
-		ea += 1
-	return str
+    str = ""
+    while idc.Byte(ea) != 0:
+        str = str + "%c" % idc.Byte(ea)
+        ea += 1
+    return str
 # End of get_asciiz()
 
 def undefBytes(ea, length) :
-	curr = ea
-	while curr < ea + length :
-		idc.MakeUnkn(curr, 0)
-		curr += 1
+    curr = ea
+    while curr < ea + length :
+        idc.MakeUnkn(curr, 0)
+        curr += 1
 # End of undefBytes()
 
 def getSegBoundaries(segName) :
-	s = idc.FirstSeg();
-	while s != idc.BADADDR :
-		if idc.SegName(s) == segName :
-			return (s, idc.SegEnd(s))
-		s = idc.NextSeg(s)
-	return None
+    s = idc.FirstSeg();
+    while s != idc.BADADDR :
+        if idc.SegName(s) == segName :
+            return (s, idc.SegEnd(s))
+        s = idc.NextSeg(s)
+    return None
 # End of getSegBoundaries()
 
--- Msvc/Rtti.py
+++ Msvc/Rtti.py
@@ -1,5 +1,6 @@
 import idc
 import idaapi
+import string
 from IDAHacks import *
 
 class RttiError(Exception) :
@@ -10,8 +11,11 @@ class RttiError(Exception) :
 
 class PMD :
     def __init__(self, mdisp, pdisp, vdisp) :
+        # member displacement
         self.mdisp = mdisp
+        # vbtable displacement
         self.pdisp = pdisp
+        # displacement inside vbtable
         self.vdisp = vdisp
 
 #
@@ -46,6 +50,19 @@ if id == 4294967295 :
     idc.AddStrucMember(id,"numBaseClasses", 0X8,    0x20000400, -1, 4);
     idc.AddStrucMember(id,"pBaseClassArray",    0XC,    0x25500400, 0XFFFFFFFF, 4,  0XFFFFFFFF, 0,  0x000002);
 
+# Used by e.g. VC8
+id = idc.GetStrucIdByName("_s__RTTIBaseClassDescriptor");
+if id == 4294967295 :
+    # print "Defining _s__RTTIBaseClassDescriptor struct."
+    id = idc.AddStrucEx(-1, "_s__RTTIBaseClassDescriptor", 0);
+    idc.AddStrucMember(id,"pTypeDescriptor",    0,  0x25500400, 0XFFFFFFFF, 4,  0XFFFFFFFF, 0,  0x000002);
+    idc.AddStrucMember(id,"numContainedBases",  0X4,    0x20000400, -1, 4);
+    idc.AddStrucMember(id,"mdisp",  0X8,    0x20200400, -1, 4);
+    idc.AddStrucMember(id,"pdisp",  0XC,    0x20200400, -1, 4);
+    idc.AddStrucMember(id,"vdisp",  0X10,   0x20200400, -1, 4);
+    idc.AddStrucMember(id,"attributes", 0X14,   0x20000400, -1, 4);
+
+# Used by e.g. VC10
 id = idc.GetStrucIdByName("_s__RTTIBaseClassDescriptor2");
 if id == 4294967295 :
     # print "Defining _s__RTTIBaseClassDescriptor2 struct."
@@ -69,20 +86,18 @@ class TypeDescriptor :
 
     def __init__(self, ea) :
         self.ea = ea
-        if not TypeDescriptor.isDefined(ea) :
-            TypeDescriptor.define(ea)
+        TypeDescriptor.define(ea)
     
     def __eq__(self, other) :
-        # There won't be two identical type descriptors.
         if type(other) is not type(self) :
             return False
         return self.ea == other.ea
 
     def __ne__(self, other) :
-        return not self.eq(other)
+        return not self.__eq__(other)
 
     def __str__(self) :
-        return "RTTI Type Descriptor for `%s'" % (self.typeName)
+        return "%08X: RTTI Type Descriptor for `%s'" % (self.ea, self.typeName)
 
     @property
     def mangledName(self) :
@@ -91,18 +106,18 @@ class TypeDescriptor :
     @property
     def baseMangledName(self) :
         name = self.mangledName
-        if name[:2] != '.?A' :
+        if name[:2] != '?A' :
             raise RttiError("Cannot get a base name for non-aggregate type `%s'" % name)
         if name[3] == 'W' :
-            return name[5:]
-        return name[4:]
+            return name[4:]
+        return name[3:]
 
     @property
     def typeName(self) :
         return idaapi.demangle_name('?x@@3' + self.mangledName + 'A', 0)[:-2]
     
-    # ".?AV", ".PAV", ".?AU", ".PAU", ".?AT", ".PAT" ".?AW"
-    typeNameMarkers = (0x56413F2E, 0x5641502E, 0x55413F2E, 0x5541502E, 0x54413F2E, 0x5441502E, 0x57413F2E)
+    # ".?AV", ".PAV", ".?AU", ".PAU", ".?AT", ".PAT" ".?AW" ".PAX"
+    typeNameMarkers = (0x56413F2E, 0x5641502E, 0x55413F2E, 0x5541502E, 0x54413F2E, 0x5441502E, 0x57413F2E, 0x5841502E)
     
     @staticmethod
     def isTypeDescriptor(ea) :
@@ -119,29 +134,42 @@ class TypeDescriptor :
         return True
     
     @staticmethod
+    def makeName(ea) :
+        locName = '??_R0' + getAsciiz(ea + 9) + '@8'
+        if idaapi.demangle_name(locName, 0) == '' :
+            raise RttiError("%08X: Location name failed to properly demangle: `%s'" % (ea, locName))
+        return locName
+        
+    @staticmethod
     def isDefined(ea) :
-        # TODO: actually verify the data is properly defined in IDB.
-        return False
+        flags = idaapi.getFlags(ea)
+        if not idc.isStruct(flags) :
+            return False
+        if not idc.isHead(flags) :
+            return False
+        if idaapi.get_name(idaapi.BADADDR, ea) != TypeDescriptor.makeName(ea) :
+            return False
+        return True
 
     @staticmethod
     def define(ea) :
-        # TODO: sanity checks
+        if TypeDescriptor.isDefined(ea) :
+            return
+
+        print "%08X: defining TypeDescriptor" % ea
+        if idaapi.get_full_long(ea + 4) != 0 :
+            raise RttiError("%08X: spare not zero; doesn't look like a correct TypeDescriptor" % (ea))
         if not TypeDescriptor.isTypeDescriptor(ea) :
-            raise RttiError("This doesn't look like a type descriptor.")
-        
+            raise RttiError("%08X: This doesn't look like a type descriptor." % ea)
+
         # FIXME: 64-bit compatibility
         mangledName = getAsciiz(ea + 8)
-
-        # Use the mangled name.
-        locName = '??_R0' + mangledName[1:] + '@8'
-        if idaapi.demangle_name(locName, 0) == '' :
-            raise RttiError("Location name failed to properly demangle: `%s'" % locName)
 
         # Define data in DB
         structLength = 8 + len(mangledName) + 1
         idaapi.do_unknown_range(ea, structLength, idaapi.DOUNK_DELNAMES)
         idaapi.doStruct(ea, structLength, idaapi.get_struc_id("_TypeDescriptor"))
-        idaapi.set_name(ea, locName, 0)
+        idaapi.set_name(ea, TypeDescriptor.makeName(ea), 0)
 
     @staticmethod
     def findAll(searchBounds) :
@@ -173,6 +201,20 @@ class ClassHierarchyDescriptor :
 
     def __init__(self, ea) :
         self.ea = ea
+        self.cachedBaseList = None
+        ClassHierarchyDescriptor.define(ea)
+
+    def __eq__(self, other) :
+        if type(other) is not type(self) :
+            return False
+        return self.ea == other.ea
+
+    def __ne__(self, other) :
+        return not self.__eq__(other)
+
+    def __str__(self) :
+        td = self.baseClassDescriptor[0].typeDescriptor
+        return "%08X: RTTI Class Hierarchy Descriptor for `%s'" % (self.ea, td.typeName)
 
     @property
     def attributes(self) :
@@ -195,34 +237,63 @@ class ClassHierarchyDescriptor :
         # FIXME: 64-bit compatibility
         # Try caching the result...
         if self.cachedBaseList is None :
-            baseArrayEa = idaapi.get_full_long(self.ea + 12)
-            self.cachedBaseList = [BaseClassDescriptor(bcdEa) for bcdEa in range(baseArrayEa, baseArrayEa + self.baseClassCount * 4, 4)]
+            bcdEas = ClassHierarchyDescriptor.getBcdEaList(self.ea)
+            self.cachedBaseList = [BaseClassDescriptor(bcdEa) for bcdEa in bcdEas]
         return self.cachedBaseList
+        
+    @staticmethod
+    def getBcdEaList(ea) :
+        baseCount = idaapi.get_full_long(ea + 8)
+        baseArrayEa = idaapi.get_full_long(ea + 12)
+        return [idaapi.get_full_long(ea) for ea in range(baseArrayEa, baseArrayEa + baseCount * 4, 4)]
 
     @staticmethod
     def isDefined(ea) :
-        # TODO: actually verify the data is properly defined in IDB.
-        return False
+        flags = idaapi.getFlags(ea)
+        if not idc.isStruct(flags) :
+            return False
+        if not idc.isHead(flags) :
+            return False
+        # TODO: verify the actual struct type.
+        return True
 
     @staticmethod
     def define(ea) :
+        if ClassHierarchyDescriptor.isDefined(ea) :
+            return
+        
+        if idaapi.get_full_long(ea) != 0 :
+            raise RttiError("%08X: signature not zero; doesn't look like a correct ClassHierarchyDescriptor" % (ea))
+        # TODO: more sanity checks
+
+        print "%08X: defining ClassHierarchyDescriptor" % ea
         strid = idaapi.get_struc_id('_s__RTTIClassHierarchyDescriptor')
         size = idaapi.get_struc_size(strid)
         idaapi.do_unknown_range(ea, size, idaapi.DOUNK_DELNAMES)
         idaapi.doStruct(ea, size, strid)
-        # TODO: name the struct.
-        # ??_R3 + ???
-    
+
         baseCount = idaapi.get_full_long(ea + 8)
         if baseCount == 0 or baseCount > 256 :
-            raise RttiError('Bogus base class count value: %d' % baseCount)
-        
+            raise RttiError('%08X: Bogus base class count value: %d' % (ea, baseCount))
+
         # FIXME: 64-bit compatibility
         baseArrayEa = idaapi.get_full_long(ea + 12)
         idaapi.do_unknown_range(baseArrayEa, baseCount * 4, idaapi.DOUNK_DELNAMES)
         idaapi.doDwrd(baseArrayEa, 4)
-        idaapi.do_data_ex(baseArrayEa, idaapi.getFlags(baseArrayEa), idaapi.get_item_size(baseArrayEa) * baseCount, BADADDR)
-        # TODO: name the array
+        idaapi.do_data_ex(baseArrayEa, idaapi.getFlags(baseArrayEa), idaapi.get_item_size(baseArrayEa) * baseCount, idaapi.BADADDR)
+
+        # Entry 0 describes the class itself => I can find out the class name.
+        bcdEas = ClassHierarchyDescriptor.getBcdEaList(ea)
+        entry0 = bcdEas[0]
+        bcd = BaseClassDescriptor(entry0)
+        className = bcd.typeDescriptor.baseMangledName
+        chdName = '??_R3' + className + '8'
+        idaapi.set_name(ea, chdName, 0)
+        bcaName = '??_R2' + className + '8'
+        idaapi.set_name(baseArrayEa, bcaName, 0)
+
+        for bcdEa in bcdEas :
+            BaseClassDescriptor.define(bcdEa)
 
 #
 # _s__RTTIBaseClassDescriptor2 handling.
@@ -246,6 +317,19 @@ class BaseClassDescriptor :
     
     def __init__(self, ea) :
         self.ea = ea
+        BaseClassDescriptor.define(ea)
+
+    def __eq__(self, other) :
+        if type(other) is not type(self) :
+            return False
+        return self.ea == other.ea
+
+    def __ne__(self, other) :
+        return not self.__eq__(other)
+
+    def __str__(self) :
+        pmd = self.where
+        return "%08X: RTTI Base Class Descriptor for `%s' at (%d,%d,%d,%d)" % (self.ea, self.typeDescriptor.typeName, pmd.mdisp, pmd.pdisp, pmd.vdisp, self.attributes)
 
     @property
     def typeDescriptor(self) :
@@ -270,20 +354,40 @@ class BaseClassDescriptor :
         return ClassHierarchyDescriptor(chd)
         
     @staticmethod
+    def isDefined(ea) :
+        flags = idaapi.getFlags(ea)
+        if not idc.isStruct(flags) :
+            return False
+        if not idc.isHead(flags) :
+            return False
+        # TODO: verify the actual struct type.
+        return True
+
+    @staticmethod
     def define(ea) :
+        if BaseClassDescriptor.isDefined(ea) :
+            return
+
+        at = idaapi.get_full_long(ea)
+        TypeDescriptor.define(at)
+
+        print "%08X: defining BaseClassDescriptor" % ea
         # TODO: fix compiler variations.
         strid = idaapi.get_struc_id('_s__RTTIBaseClassDescriptor2')
         size = idaapi.get_struc_size(strid)
         idaapi.do_unknown_range(ea, size, idaapi.DOUNK_DELNAMES)
         idaapi.doStruct(ea, size, strid)
         
-        name = "??_R1"
+        name = '??_R1'
         name += mangleNumber(getInt32(ea + 8))
         name += mangleNumber(getInt32(ea + 12))
         name += mangleNumber(getInt32(ea + 16))
         name += mangleNumber(idaapi.get_full_long(ea + 20))
-        name += TypeDescriptor(idaapi.get_full_long(ea)).baseMangledName + "8"
+        name += TypeDescriptor(idaapi.get_full_long(ea)).baseMangledName + '8'
         idaapi.set_name(ea, name, 0)
+
+        at = idaapi.get_full_long(ea + 16)
+        ClassHierarchyDescriptor.define(at)
 
 #
 # _s__RTTICompleteObjectLocator handling.
@@ -298,6 +402,18 @@ class CompleteObjectLocator :
     
     def __init__(self, ea) :
         self.ea = ea
+        CompleteObjectLocator.define(ea)
+
+    def __eq__(self, other) :
+        if type(other) is not type(self) :
+            return False
+        return self.ea == other.ea
+
+    def __ne__(self, other) :
+        return not self.__eq__(other)
+
+    def __str__(self) :
+        return '%08X: RTTI Complete Object Locator' % (self.ea)
 
     @property
     def vftableOffset(self) :
@@ -318,10 +434,50 @@ class CompleteObjectLocator :
         return ClassHierarchyDescriptor(at)
 
     @staticmethod
+    def isDefined(ea) :
+        flags = idaapi.getFlags(ea)
+        if not idc.isStruct(flags) :
+            return False
+        if not idc.isHead(flags) :
+            return False
+        # TODO: verify the actual struct type.
+        return True
+
+    @staticmethod
     def define(ea) :
+        if CompleteObjectLocator.isDefined(ea) :
+            return
+
+        if idaapi.get_full_long(ea) != 0 :
+            raise RttiError("%08X: signature not zero; doesn't look like a correct CompleteObjectLocator" % (ea))
+        # TODO: more sanity checks
+
+        # Ensure referenced structs are defined.
+        # An exception will be thrown if something goes wrong.
+        at = idaapi.get_full_long(ea + 12)
+        TypeDescriptor.define(at)
+        at = idaapi.get_full_long(ea + 16)
+        ClassHierarchyDescriptor.define(at)
+
+        print "%08X: defining CompleteObjectLocator" % ea
         strid = idaapi.get_struc_id('_s__RTTICompleteObjectLocator')
         size = idaapi.get_struc_size(strid)
         idaapi.do_unknown_range(ea, size, idaapi.DOUNK_DELNAMES)
         idaapi.doStruct(ea, size, strid)
-        pass
+        
+#
+# Integrated scanner
+#
 
+def scanRtti() :
+    dataBounds = getSegBoundaries(".data")
+    if dataBounds is None :
+        print "No .data section found. Cannot continue."
+        return False
+    
+    print "Scanning for TypeDescriptors (might take a while)..."
+    tds = TypeDescriptor.findAll(dataBounds)
+    print "Found %d descriptor(s)." % len(tds)
+    for ea in tds :
+        x = TypeDescriptor(ea)
+        print x
